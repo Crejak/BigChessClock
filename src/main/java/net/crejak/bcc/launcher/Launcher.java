09d616233c8f588687303480c78fbee4070a393b
Valentin Rigolle <valentin.rigolle@gmail.com>
2022-04-30 18:06:51 +0200

    Test config

---
+++ src/main/java/net/crejak/bcc/launcher/Launcher.java
@@ -0,0 +1,9 @@
+package net.crejak.bcc.launcher;
+
+import net.crejak.bcc.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
